18d0de173d971595532f6ddb083b1572f34fbb8b
AlexPinhasov <alex5872205@gmail.com>
2019-10-23 15:23:20 +0300

    Update EqualableGenericProtocol.swift

--- EqualableGeneric/Classes/EqualableGenericProtocol.swift
+++ EqualableGeneric/Classes/EqualableGenericProtocol.swift
@@ -7,40 +7,30 @@
 
 import Foundation
 
-public protocol EqualableGenericProtocol : MirrorableValues,Equatable
-{
+public protocol EqualableGenericProtocol : MirrorableValues,Equatable {
     func isEqual(toObject objectToCompare: MirrorableValues) -> Bool
 }
 
-extension EqualableGenericProtocol where Self:MirrorableValues
-{
-    public func isEqual(toObject objectToCompare: MirrorableValues) -> Bool
-    {
+extension EqualableGenericProtocol where Self:MirrorableValues {
+    public func isEqual(toObject objectToCompare: MirrorableValues) -> Bool {
         let selfObjectProperties      = self.mirrorableValues()
         let objectToCompareProperties = objectToCompare.mirrorableValues()
         
-        for (index,_) in selfObjectProperties.enumerated()
-        {
-            if String(describing: selfObjectProperties[index]) != String(describing: objectToCompareProperties[index])
-            {
+        for (index,_) in selfObjectProperties.enumerated() {
+            if String(describing: selfObjectProperties[index]) != String(describing: objectToCompareProperties[index]) {
                 return false
             }
         }
         return true
     }
     
-    static func == (lhs: Self, rhs: Self) -> Bool
-    {
+    static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.isEqual(toObject: rhs)
     }
 }
 
-extension NSObject: EqualableGenericProtocol
-{
-    static func == (lhs: NSObject, rhs: NSObject) -> Bool
-    {
+extension NSObject: EqualableGenericProtocol {
+    static func == (lhs: NSObject, rhs: NSObject) -> Bool {
         return lhs.isEqual(toObject: rhs)
     }
 }
-
-
